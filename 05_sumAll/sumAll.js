44a814f58466fcf9ee9555cc6318888430601aed
Jyjar <156258612+Jyjar@users.noreply.github.com>
2024-07-31 12:40:11 +0200

    Exercise 05_sumAll Done.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    let finalCount = 0;
+
+    if(start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    if(start < 0 || end < 0) {
+        return "ERROR"
+    }
+    
+    if(!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR"
+    }
+
+
+    for(start; start <= end; start++) {
+        finalCount += start;
+    }
+    return finalCount;
 
 };
 
